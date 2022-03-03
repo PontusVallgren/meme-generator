efd8e21e2f7ea37adb2ace6a5a0a4bb231bdd5b9
PontusVallgren <pontus.wallgren92@gmail.com>
2022-03-03 12:37:49 +0100

    Create meme component, with button for render new meme img and text input to display on the meme img

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import Header from "./components/Header";
 import Meme from "./components/Meme";
+import memeData from "./memeData";
 
 function App() {
+  const imgUrls = memeData.data.memes.map((items) => items.url);
+
   return (
     <div>
       <Header />
-      <Meme />
+      <Meme images={imgUrls} />
     </div>
   );
 }
--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,23 +1,59 @@
-const Meme = () => {
+import { useState } from "react";
+
+const Meme = ({ images }) => {
+  const [meme, setMeme] = useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "https://i.imgflip.com/30b1gx.jpg",
+  });
+
+  const [allMemeImages, setAllMemeImages] = useState(images);
+
+  const getMemeImage = () => {
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      randomImage: `${images[Math.floor(Math.random() * images.length)]}`,
+    }));
+  };
+
+  const handleChange = (e) => {
+    const { value, name } = e.target;
+
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      [name]: value,
+    }));
+  };
+
   return (
     <div className='meme-ctn'>
       <form className='form'>
         <input
           className='meme-input'
-          id='memeOne'
-          name='memeOne'
+          name='topText'
           type='text'
-          value='Upper text'
+          onChange={handleChange}
+          value={meme.topText}
+          placeholder='Top text'
         ></input>
         <input
           className='meme-input'
-          id='memeTwo'
-          name='memeTwo'
+          id='bottomText'
+          name='bottomText'
           type='text'
-          value='Bottom text'
+          onChange={handleChange}
+          value={meme.bottomText}
+          placeholder='Bottom text'
         ></input>
       </form>
-      <button id='render-btn'>Get a new meme image</button>
+      <button onClick={getMemeImage} id='render-btn'>
+        Get a new meme image
+      </button>
+      <div className='meme-ctn'>
+        <img className='meme-img' src={meme.randomImage}></img>
+        <p className='meme-text top'>{meme.topText}</p>
+        <p className='meme-text bottom'>{meme.bottomText}</p>
+      </div>
     </div>
   );
 };
